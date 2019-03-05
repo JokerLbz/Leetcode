dcfd1a2ead86edfc52a9d05f0821663efe204857
Jokerlbz <lbz1996@hotmail.com>
2019-03-05 14:24:00 +0800

    Update short solution on Mar 5th

--- Algorithms/036. Valid Sudoku/36. Valid Sudoku.cpp
+++ Algorithms/036. Valid Sudoku/36. Valid Sudoku.cpp
@@ -1,70 +1,28 @@
 //Question : 36. Valid Sudoku
 //URL : https://leetcode.com/problems/valid-sudoku/
-//Approach : Intuition (直接求解)
+//Approach : set (数组代替set)
 //Runtime : 20 ms (98.05%)
-//Memory Usage : 13 MB (51.47%)
-//Time complexity : O(n^2)
+//Memory Usage : 12.1 MB (96.18%)
+//Time complexity : O(1)
 //Space complexity : O(1)
+//Reference:https://leetcode.com/problems/valid-sudoku/discuss/15464/My-short-solution-by-C%2B%2B.-O(n2)
 
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<char> s;
-        //test row
-        for(int i = 0; i < 9; i++)
-        {
-            s.clear();
-            for(int j = 0; j < 9; j++)
+        int tab_row[9][9] = {0}, tab_column[9][9] = {0}, tab_sub[9][9] = {0};
+        for(int i = 0; i < board.size(); i++)
+            for(int j = 0 ; j < board[i].size(); j++)
             {
                 if(board[i][j] != '.')
                 {
-                    if(s.count(board[i][j]) > 0)
+                    int num = board[i][j] - '0' - 1;
+                    int k = i / 3 * 3 + j / 3;
+                    if(tab_row[i][num] || tab_column[j][num] || tab_sub[k][num])
                         return false;
-                    else
-                        s.insert(board[i][j]);
+                    tab_row[i][num] = tab_column[j][num] = tab_sub[k][num] = 1;
                 }
             }
-        }
-        //test column
-        for(int i = 0; i < 9; i++)
-        {
-            s.clear();
-            for(int j = 0; j < 9; j++)
-            {
-                if(board[j][i] != '.')
-                {
-                    if(s.count(board[j][i]) > 0)
-                        return false;
-                    else
-                        s.insert(board[j][i]);
-                }
-            }
-        }
-        //test sub-box
-        for(int i = 0; i < 3; i++)
-        {
-            for(int j = 0; j < 3; j++)
-                if(!test_sub_box(board, i * 3, j * 3))   return false;
-        }
-        return true;
-    }
-    
-    bool test_sub_box(vector<vector<char>>& board,int x,int y)
-    {
-        set<char> s;
-        for(int i = x; i < x+3; i++)
-        {
-            for(int j = y; j < y+3; j++)
-            {
-                if(board[i][j] != '.')
-                {
-                    if(s.count(board[i][j]) > 0)
-                        return false;
-                    else
-                        s.insert(board[i][j]);
-                }
-            }
-        }
         return true;
     }
 };
