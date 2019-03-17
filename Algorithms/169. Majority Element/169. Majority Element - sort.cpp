0c5adcc08a013ddf5fde6905c82530fc5e702422
JokerLbz <lbz1996@hotmail.com>
2019-03-17 22:47:49 +0800

    Update

--- Algorithms/169. Majority Element/169. Majority Element - sort.cpp
+++ Algorithms/169. Majority Element/169. Majority Element - sort.cpp
@@ -2,7 +2,7 @@
 //   √ 44/44 cases passed (28 ms)
 //   √ Your runtime beats 41.92 % of cpp submissions
 //   √ Your memory usage beats 55.97 % of cpp submissions (11.3 MB)
-////T/S Complexity : O(1) / O(1)
+//T/S Complexity : O(nlog n) / O(1)
 
 class Solution {
 public:
